90f6f397bd35333089d6baeadeef1dccfa16e29e
IStolen <ingvild.stoelen@gmail.com>
2020-10-04 11:59:34 +0200

    xx

---
+++ src/components/layout.js
@@ -0,0 +1,25 @@
+import React from "react"
+import ButtonAppBar from "./header.js"
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const nrtheme = createMuiTheme({
+  palette: {
+    primary: { 
+      main: '#255839',
+    },
+    secondary: {
+      main: '#F3FAA0',
+    },
+  },     
+});
+
+export default function Layout({ children }) {
+  return (
+    <ThemeProvider theme={nrtheme}>
+    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <ButtonAppBar/>
+      {children}
+    </div>
+    </ThemeProvider>
+  )
+}
---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Contact() {
+  return (
+    <Layout>
+      <h1>We'd love to talk! Email us at the address below</h1>
+      <p>
+        <a href="mailto:nr@example.com">NR@example.com</a>
+      </p>
+    </Layout>
+  )
+}
